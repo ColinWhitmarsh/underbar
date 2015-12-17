60243feab7b828f98c0c73ae86629cf7f526b44b
ColinWhitmarsh <colin.whitmarsh@gmail.com>
2015-12-17 12:35:49 -0800

    filter completed

--- src/underbar.js
+++ src/underbar.js
@@ -79,6 +79,14 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    
+    _.each(collection, function(val,key,col){
+      if(test(val,key,col))
+        result.push(val);
+    });
+
+    return result
   };
 
   // Return all elements of an array that don't pass a truth test.
